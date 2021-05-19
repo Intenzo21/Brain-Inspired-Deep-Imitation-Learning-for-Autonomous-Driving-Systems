c092d5c9e3085d68094032663e4f799abfb0f817
Intenzo21 <Casp3rmail@gmail.com>
2021-05-19 22:59:09 +0300

    Revert "Add files via upload"
    This reverts commit 41c0a95014dd9fc7927d613496dc127e54a1c9b5.

--- models.py
+++ models.py
@@ -1,43 +1,17 @@
-"""
-Define the CNN benchmark and hybrid model architectures.
-
-This script comprise the code implementations of the developed neural networks.
-"""
 from keras.models import Sequential, Model
-from keras.layers import Lambda, Conv2D, Dropout, Dense, Flatten, Reshape, RNN, Input, Add, MaxPool2D
-from kerasncp.ltc_cell import LTCCell
+from keras.layers import Lambda, Conv2D, Dropout, Dense, Flatten, Reshape, RNN, Input, Add
+from kerasncp.tf import LTCCell
 from kerasncp.wirings import NCP
 from keras.utils import plot_model
-import seaborn as sns
-import matplotlib.pyplot as plt
-from constants import INPUT_SHAPE
+
+from utils import INPUT_SHAPE
 
 
-def plot_wiring(cell):
-    """
-    Plot the NCP wiring.
-
-    :param cell: NCP cell
-    :return: None
+def NVIDIA_model():
     """
 
-    sns.set_style("white")
-    plt.figure(figsize=(12, 12))
-    legend_handles = cell.draw_graph(layout='spiral', neuron_colors={"command": "tab:cyan"})
-    plt.legend(handles=legend_handles, loc="upper center", bbox_to_anchor=(1, 1))
-    sns.despine(left=True, bottom=True)
-    plt.tight_layout()
-    plt.show()
-
-
-def cnn_model():
+    :return:
     """
-    Define the Keras sequential Nvidia CNN benchmark architecture.
-
-    :return: the CNN reference model
-    """
-
-    # Build the sequential CNN model
     model = Sequential()
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=INPUT_SHAPE))
     model.add(Conv2D(24, 5, 2, activation='relu'))
@@ -45,27 +19,46 @@ def cnn_model():
     model.add(Conv2D(48, 5, 2, activation='relu'))
     model.add(Conv2D(64, 3, activation='relu'))
     model.add(Conv2D(64, 3, activation='relu'))
+    # model.add(Conv2D(64, 3, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
-    # plot_model(model, show_shapes=True)
     model.summary()
+    plot_model(model, show_shapes=True)
     return model
 
 
-def cnn_ncp_model():
+def other_model():
     """
-    Define the Keras sequential CNN-NCP hybrid architecture.
-
-    A CNN feature extractor is stacked with an NCP RNN temporal modelling structure.
-
-    :return: the CNN-NCP model
+    Random (personally developed) CNN model for testing.
+    :return: sequential CNN model
     """
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 127.5 - 1.,
+                     input_shape=INPUT_SHAPE,
+                     output_shape=INPUT_SHAPE))
+    model.add(Conv2D(8, 3, 2, padding="same", activation='relu'))
+    model.add(Conv2D(16, 3, 2, padding="same", activation='relu'))
+    model.add(Conv2D(32, 3, 2, padding="same", activation='relu'))
+    model.add(Conv2D(64, 3, 2, padding="same", activation='relu'))
+    model.add(Flatten())
+    model.add(Dropout(.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(1, activation='relu'))
+    model.summary()
+    plot_model(model, show_shapes=True)
+    return model
 
-    # Set the NCP wiring
+
+def stacked_model():
+    """
+    CNN+NCP stacked model.
+    :return: sequential stacked model
+    """
     wiring = NCP(
         inter_neurons=12,  # Number of inter neurons
         command_neurons=8,  # Number of command neurons
@@ -76,11 +69,8 @@ def cnn_ncp_model():
         # command neuron layer
         motor_fanin=6,  # How many incoming synapses has each motor neuron
     )
+    rnn_cell = LTCCell(wiring)
 
-    # Create the the NCP cell based on the LTC neuron.
-    ncp_cell = LTCCell(wiring)
-
-    # Build the sequential hybrid model
     model = Sequential()
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=INPUT_SHAPE))
     model.add(Conv2D(24, 5, 2, activation='relu'))
@@ -88,53 +78,53 @@ def cnn_ncp_model():
     model.add(Conv2D(48, 5, 2, activation='relu'))
     model.add(Conv2D(64, 3, activation='relu'))
     model.add(Conv2D(64, 3, activation='relu'))
+    # model.add(Conv2D(64, 3, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
+    # model.add(Dense(50, activation='relu'))
+    # model.add(Dense(32, activation='relu'))
     model.add(Reshape((1, -1)))
-    model.add(RNN(ncp_cell, unroll=True))
+    model.add(RNN(rnn_cell, unroll=True))
     model.summary()
-    # plot_model(model, show_shapes=True)
+    plot_model(model, show_shapes=True)
     return model
 
 
-def cnn_dncp_model():
+# Functional dual NCP model
+# As close as possible to the two brain wiring drawings
+# gives really high loss value on the comma.ai dataset
+def stacked_dual_model():
     """
-    Define the blended functional CNN-DNCP v2 model.
-
-    Two NCP wiring settings are adopted representing the left and right sides of the human brain.
-    This model is, therefore, called DNCP which is short for Dual NCP.
-
-    :return: the brain-inspired CNN-DNCP model
+    Stacked functional CNN and dual NCP model.
+    Two NCP wiring settings representing the left and right sides of the human brain.
+    The
+    :return:
     """
-
-    # Set the NCP wiring configurations
     left_wiring = NCP(
-        inter_neurons=3,
-        command_neurons=5,
-        motor_neurons=1,
-        sensory_fanout=2,
-        inter_fanout=2,
-        recurrent_command_synapses=1,
-        motor_fanin=2,
+        inter_neurons=3,  # Number of inter neurons
+        command_neurons=5,  # Number of command neurons
+        motor_neurons=1,  # (1 output) Number of motor neurons
+        sensory_fanout=2,  # How many outgoing synapses has each sensory neuron
+        inter_fanout=2,  # (average=15/8) How many outgoing synapses has each inter neuron
+        recurrent_command_synapses=1,  # Now many recurrent synapses are in the
+        # command neuron layer
+        motor_fanin=2,  # (12/5) How many incoming synapses has each motor neuron
     )
 
     right_wiring = NCP(
-        inter_neurons=4,
-        command_neurons=6,
-        motor_neurons=1,
-        sensory_fanout=2,
-        inter_fanout=2,
-        recurrent_command_synapses=2,
-        motor_fanin=2,
+        inter_neurons=4,  # Number of inter neurons
+        command_neurons=6,  # Number of command neurons
+        motor_neurons=1,  # Number of motor neurons
+        sensory_fanout=2,  # (18/8) How many outgoing synapses has each sensory neuron
+        inter_fanout=2,  # How many outgoing synapses has each inter neuron
+        recurrent_command_synapses=2,  # Now many recurrent synapses are in the
+        # command neuron layer
+        motor_fanin=2,  # How many incoming synapses has each motor neuron
     )
-
-    # Create the left and right brain-inspired NCP cells.
-    # These are based on the LTC neuron.
     ncp_cell_left = LTCCell(left_wiring)
     ncp_cell_right = LTCCell(right_wiring)
 
-    # Build the functional brain-inspired hybrid model
     input_lyr = Input(shape=INPUT_SHAPE)
     lambda_lyr = Lambda(lambda x: x / 127.5 - 1.0)(input_lyr)
     conv_1 = Conv2D(24, 5, 2, activation='relu')(lambda_lyr)
@@ -156,43 +146,46 @@ def cnn_dncp_model():
     return model
 
 
-def cnn_dncp_v2_model():
+# Functional dual NCP model
+# Kept the right wiring as given in the Google Colaboratory link below.
+# https://colab.research.google.com/drive/1-mZunxqVkfZVBXNPG0kTSKUNQUSdZiBI?usp=sharing#scrollTo=3phJCJCa894e
+# The replicated 'left brain' wiring was adjusted taking into account the right side one.
+# That is in the brain wiring drawings sent to me by my supervisor showed 4 and 3 inter and
+# 6 and 5 command neurons of the right and left sides respectively.
+# Therefore, to preserve this ratio, I multiplied the Colab inter neurons by 3/4 and command neurons by 5/6 to get
+# the count for the left side wiring replication. The resultant model is provided below.
+def stacked_dual_model_v2():
     """
-    Define the blended functional version 2 of the CNN-DNCP model.
-
-    This model is built the same way as the original CNN-DNCP but with
-    modified NCP wiring configurations.
-
-    :return: the CNN-DNCP v2 model
+    Stacked functional CNN and dual NCP model v2.
+    Two NCP wiring settings representing the left and right sides of the human brain.
+    The
+    :return:
     """
-
-    # Set the NCP wirings
     left_wiring = NCP(
-        inter_neurons=9,
-        command_neurons=7,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=4,
-        recurrent_command_synapses=4,
-        motor_fanin=6,
+        inter_neurons=9,  # Number of inter neurons
+        command_neurons=7,  # Number of command neurons
+        motor_neurons=1,  # Number of motor neurons
+        sensory_fanout=4,  # How many outgoing synapses has each sensory neuron
+        inter_fanout=4,  # How many outgoing synapses has each inter neuron
+        recurrent_command_synapses=4,  # Now many recurrent synapses are in the
+        # command neuron layer
+        motor_fanin=6,  # How many incoming synapses has each motor neuron
     )
 
     right_wiring = NCP(
-        inter_neurons=12,
-        command_neurons=8,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=4,
-        recurrent_command_synapses=4,
-        motor_fanin=6,
+        inter_neurons=12,  # Number of inter neurons
+        command_neurons=8,  # Number of command neurons
+        motor_neurons=1,  # Number of motor neurons
+        sensory_fanout=4,  # How many outgoing synapses has each sensory neuron
+        inter_fanout=4,  # How many outgoing synapses has each inter neuron
+        recurrent_command_synapses=4,  # Now many recurrent synapses are in the
+        # command neuron layer
+        motor_fanin=6,  # How many incoming synapses has each motor neuron
     )
 
-    # Create the left and right brain-inspired NCP cells.
-    # These are based on the LTC neuron.
     ncp_cell_left = LTCCell(left_wiring)
     ncp_cell_right = LTCCell(right_wiring)
 
-    # Build the functional brain-inspired hybrid model
     input_lyr = Input(shape=INPUT_SHAPE)
     lambda_lyr = Lambda(lambda x: x / 127.5 - 1.0)(input_lyr)
     conv_1 = Conv2D(24, 5, 2, activation='relu')(lambda_lyr)
@@ -210,120 +203,5 @@ def cnn_dncp_v2_model():
     output = Lambda(lambda x: x * 0.5)(ncps_added)
     model = Model(inputs=input_lyr, outputs=output)
     model.summary()
-
-    return model
-
-
-def cnn_dncp_v3_model():
-    """
-    Define the blended functional version 3 of the CNN-DNCP model.
-
-    This model is built the same way as the original CNN-DNCP but with
-    modified NCP wiring configurations.
-
-    :return: the CNN-DNCP v3 model
-    """
-
-    left_wiring = NCP(
-        inter_neurons=12,
-        command_neurons=8,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=4,
-        recurrent_command_synapses=4,
-        motor_fanin=6,
-    )
-
-    right_wiring = NCP(
-        inter_neurons=5,
-        command_neurons=3,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=2,
-        recurrent_command_synapses=2,
-        motor_fanin=2,
-    )
-
-    # Create the left and right brain-inspired NCP cells.
-    # These are based on the LTC neuron.
-    ncp_cell_left = LTCCell(left_wiring)
-    ncp_cell_right = LTCCell(right_wiring)
-
-    # Build the functional brain-inspired hybrid model
-    input_lyr = Input(shape=INPUT_SHAPE)
-    lambda_lyr = Lambda(lambda x: x / 127.5 - 1.0)(input_lyr)
-    conv_1 = Conv2D(24, 5, 2, activation='relu')(lambda_lyr)
-    conv_2 = Conv2D(36, 5, 2, activation='relu')(conv_1)
-    conv_3 = Conv2D(48, 5, 2, activation='relu')(conv_2)
-    conv_4 = Conv2D(64, 3, activation='relu')(conv_3)
-    conv_5 = Conv2D(64, 3, activation='relu')(conv_4)
-    dropout_lyr = Dropout(0.5)(conv_5)
-    flatten_lyr = Flatten()(dropout_lyr)
-    dense_1 = Dense(100, activation='relu')(flatten_lyr)
-    reshape_lyr = Reshape((1, -1))(dense_1)
-    ncp_left = RNN(ncp_cell_left, unroll=True)(reshape_lyr)
-    ncp_right = RNN(ncp_cell_right, unroll=True)(reshape_lyr)
-    ncps_added = Add()([ncp_left, ncp_right])
-    output = Lambda(lambda x: x * 0.5)(ncps_added)
-    model = Model(inputs=input_lyr, outputs=output)
-    model.summary()
-
-    return model
-
-
-def cnn_dncp_v4_model():
-    """
-    Define the blended functional version 4 of the CNN-DNCP model.
-
-    In this model, two of the CNN-DNCP CNN layers are removed to make way
-    for the two MaxPool2D layers to reduce its complexity.
-    The same NCP wiring settings are used as in the CNN-DNCP v4 model.
-
-    :return: the CNN-DNCP v4 model
-    """
-
-    left_wiring = NCP(
-        inter_neurons=12,
-        command_neurons=8,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=4,
-        recurrent_command_synapses=4,
-        motor_fanin=6,
-    )
-
-    right_wiring = NCP(
-        inter_neurons=5,
-        command_neurons=3,
-        motor_neurons=1,
-        sensory_fanout=4,
-        inter_fanout=2,
-        recurrent_command_synapses=2,
-        motor_fanin=2,
-    )
-
-    # Create the left and right brain-inspired NCP cells.
-    # These are based on the LTC neuron.
-    ncp_cell_left = LTCCell(left_wiring)
-    ncp_cell_right = LTCCell(right_wiring)
-
-    # Build the functional brain-inspired hybrid model
-    input_lyr = Input(shape=INPUT_SHAPE)
-    lambda_lyr = Lambda(lambda x: x / 127.5 - 1.0)(input_lyr)
-    conv_1 = Conv2D(24, 5, 2, activation='relu')(lambda_lyr)
-    max_1 = MaxPool2D()(conv_1)
-    conv_2 = Conv2D(48, 5, 2, activation='relu')(max_1)
-    max_2 = MaxPool2D()(conv_2)
-    conv_3 = Conv2D(64, 3, activation='relu')(max_2)
-    dropout_lyr = Dropout(0.5)(conv_3)
-    flatten_lyr = Flatten()(dropout_lyr)
-    dense_1 = Dense(100, activation='relu')(flatten_lyr)
-    reshape_lyr = Reshape((1, -1))(dense_1)
-    ncp_left = RNN(ncp_cell_left, unroll=True)(reshape_lyr)
-    ncp_right = RNN(ncp_cell_right, unroll=True)(reshape_lyr)
-    ncps_added = Add()([ncp_left, ncp_right])
-    output = Lambda(lambda x: x * 0.5)(ncps_added)
-    model = Model(inputs=input_lyr, outputs=output)
-    # model.summary()
-    # plot_model(model, show_shapes=True)
+    plot_model(model, show_shapes=True)
     return model
--- utils.py
+++ utils.py
@@ -1,278 +1,90 @@
-"""
-Udacity dataset general functions.
-
-These are adopted in training, testing the models and also driving the
-Udacity simulator autonomous car.
-"""
-import os
-import random
 import numpy as np
-import pandas as pd
-import csv
-import cv2
-from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import os
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.optimizers import Adam
 
-from constants import IMAGE_HEIGHT, IMAGE_CHANNELS, IMAGE_WIDTH, TRAIN_DATA_DIR, B_SIZE, NB_EPOCHS, LR
+INPUT_SHAPE = (80, 120, 3)  # Image input shape
 
 
-def load_data(labels_fl, test_size=None):
+def scale_data(data, ds_min=None, ds_max=None):
     """
-    Load and split the data into training and testing sets.
-
-
-    :param labels_fl: directory of the labels (measurement logs) CSV file
-    :param test_size: size of the testing set
-    :return: training and testing input and output sets if a test_size parameter is provided
-    otherwise return without splitting
+    Scale (normalize) the data.
+    :param ds_min:
+    :param ds_max:
+    :param data: data list
+    :return: normalized data list
     """
-
-    labels = pd.read_csv(labels_fl)
-    x = labels[['center', 'left', 'right']].values
-    y = labels['steering'].values
-
-    # R
-    if test_size:
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=0)
-        return x_train, x_test, y_train, y_test
-    return x, y
+    # Check if minimum and maximum dataset value parameters are provided
+    # If not take the current dataset file minimum and maximum values
+    if all(i is None for i in [ds_min, ds_max]):
+        ds_min = np.min(data)
+        ds_max = np.max(data)
+    data = (data - ds_min) / (ds_max - ds_min)
+    return data
 
 
-def load_image(dt_dir, image_file):
+def standardize_data(data):
+    data_mean = np.mean(data)
+    data_std = np.std(data)
+    data = (data - data_mean) / data_std
+    return data
+
+
+def save_data(file_name, **kwargs):
     """
-    Load an RGB image.
-
-    :param dt_dir: Images directory
-    :param image_file: Image file name
-    :return: RGB image
+    Save the data to a NumPy zip file.
+    :param file_name:
+    :param kwargs:
+    :return:
     """
+    np.savez(file_name, **kwargs)
 
-    return mpimg.imread(os.path.join(dt_dir, image_file.strip()))
 
-
-def preprocess(img):
+def npy_chunks(data, outer_folder, inner_folder, chunk_size=32, chunk_start=0):
     """
-    Preprocess the input image by cropping, resizing and converting to YUV colour space.
-
-    :param img: input image
-    :return: preprocessed image
+    Divide data into chunks.
+    :param data: data file
+    :param folder_name: name of the folder where the chunks will be stored
+    :param file_name: name of the chunk file
+    :param chunk_size: size of each chunk of data
+    :param chunk_start: used in enumerating the chunks (starting number for the file name)
+    :return: number of the last chunk that has been saved
     """
+    # chunk_start += 1
+    file_name = f'{chunk_size}_{inner_folder}'
+    path = f'{chunk_size}_batched_{outer_folder}/{inner_folder}'
+    saved = 0
+    for idx, i in tqdm(enumerate(range(0, data.shape[0], chunk_size))):
+        chunk = data[i:i + chunk_size]
+        # Discard the last chunk if it is less than the chunk (batch) size.
+        # That is to keep the npy file sizes consistent.
+        # Worst case a loss of (batch size * num of files) - num of files frames
+        # Number of frames in the whole dataset: 522434
+        # Number of files: 11
+        # Batch size: 64
+        # Frames lost (worst case)=(64 * 11) - 11=693
+        # Percentage ~ 0.133% < 1%
+        if chunk.shape[0] < chunk_size:
+            continue
+        try:
+            np.save(f'{path}/{file_name}_{idx + chunk_start}', chunk)
+        except FileNotFoundError:
+            os.makedirs(f'{path}')
+            np.save(f'{path}/{file_name}_{idx + chunk_start}', chunk)
 
-    # Crop the image
-    img = img[60:-25, :, :]
-
-    # Resize the image
-    img = cv2.resize(img, (200, 66), cv2.INTER_AREA)
-
-    # Convert the image to YUV
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-
-    return img
-
-
-# Data augmentation functions
-
-def random_adjust(data_dir, center, left, right, steering_angle):
-    """
-    Adjust the steering angle of a random image.
-
-    :param data_dir: images directory
-    :param center: center view image
-    :param left: left view image
-    :param right: right view image
-    :param steering_angle: the steering angle related to the input frame
-    :return: random image and its corresponding steering angle after adjustment
-    """
-
-    choice = np.random.choice(3)
-    if choice == 0:
-        return load_image(data_dir, left), steering_angle + 0.2
-    elif choice == 1:
-        return load_image(data_dir, right), steering_angle - 0.2
-    return load_image(data_dir, center), steering_angle
+        saved = idx
+    return saved + 1  # To avoid same chunk file name issues (last and first chunks
+    # from 2 consecutive .h5 recording files i.e. last chunk file name is 3060 so we increment by 1 in order to start
+    # from 3061)
 
 
-def random_flip(image, steering_angle):
-    """
-    Flip the input image horizontally and perform a steering angle adjustment at random.
-
-    :param image: input frame
-    :param steering_angle: steering angle related to the input frame
-    :return: flipped input frame and adjusted steering angle
-    """
-
-    if np.random.rand() < 1:
-        image = cv2.flip(image, 1)
-        steering_angle = -steering_angle
-    return image, steering_angle
-
-
-def random_shift(image, steering_angle, range_x, range_y):
-    """
-    Shift (translate) the input image and perform a steering angle adjustment.
-
-    :param image: input frame
-    :param steering_angle: steering angle related to the input frame
-    :param range_x: horizontal shift range
-    :param range_y: vertical shift range
-    :return: shifted version of the input frame and steering angle
-    """
-
-    trans_x = range_x * (np.random.rand() - 0.5)
-    trans_y = range_y * (np.random.rand() - 0.5)
-    steering_angle += trans_x * 0.002
-    trans_m = np.float32([[1, 0, trans_x], [0, 1, trans_y]])
-    height, width = image.shape[:2]
-    image = cv2.warpAffine(image, trans_m, (width, height))
-    return image, steering_angle
-
-
-def random_shadow(image):
-    """
-    Add shadow to the input frame.
-
-    :param image: input frame
-    :return: shaded input frame
-    """
-
-    bright_factor = 0.3
-    x = random.randint(0, image.shape[1])
-    y = random.randint(0, image.shape[0])
-    width = random.randint(image.shape[1], image.shape[1])
-    if x + width > image.shape[1]:
-        x = image.shape[1] - x
-    height = random.randint(image.shape[0], image.shape[0])
-    if y + height > image.shape[0]:
-        y = image.shape[0] - y
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    image[y:y + height, x:x + width, 2] = image[y:y + height, x:x + width, 2] * bright_factor
-    return cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
-
-
-def random_brightness(image):
-    """
-    Alter the brightness of the input image.
-
-    :param image: input frame
-    :return: altered input image
-    """
-
-    # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    ratio = 1.0 + (np.random.rand() - 0.5)
-    hsv[:, :, 2] = hsv[:, :, 2] * ratio
-    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-
-
-def augment(data_dir, center, left, right, steering_angle, range_x=100, range_y=10):
-    """
-    Generate an augmented image and adjust the associated steering angle.
-
-    :param data_dir: images directory
-    :param center: center view image
-    :param left: left view image
-    :param right: right view image
-    :param steering_angle: the steering angle related to the input frame
-    :param range_x: horizontal translation range
-    :param range_y: vertical translation range
-    :return: modified version of the input frame and steering angle
-    """
-
-    image, steering_angle = random_adjust(data_dir, center, left, right, steering_angle)
-    image, steering_angle = random_flip(image, steering_angle)
-    image, steering_angle = random_shift(image, steering_angle, range_x, range_y)
-    image = random_shadow(image)
-    image = random_brightness(image)
-    return image, steering_angle
-
-
-def batcher(dt_dir, image_paths, steering_angles, batch_size, training_flag, to_csv=False):
-    """
-    Generate batches of training images from the given image paths and their associated steering angles.
-
-    :param dt_dir: the directory where the images are
-    :param image_paths: paths to the input images
-    :param steering_angles: the steering angles related to the input frames
-    :param batch_size: the batch size used to train the model
-    :param training_flag: a boolean flag to determine whether we are in training or validation mode
-    :param to_csv: a boolean flag to decide if we are augmenting data and saving the angles in a CSV file
-    :return: batches of images and steering angles
-    """
-
-    images = np.empty([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
-    steers = np.empty(batch_size)
-    permuted = np.random.permutation(image_paths.shape[0])  # Global seed set
-
-    count = 0
-    while True:
-        batch = permuted[count:count + batch_size]
-        curr_bs = batch.shape[0]
-
-        if image_paths.shape[0] <= count and to_csv:
-            break
-        # if batch.size == 0:
-        #     break
-        assert batch.size != 0
-        for idx, index in enumerate(batch):
-            center, left, right = image_paths[index]
-            steering_angle = steering_angles[index]
-            if training_flag and np.random.rand() < 0.6:  # 60% probability of augmenting the data
-                image, steering_angle = augment(dt_dir, center, left, right, steering_angle)
-            else:
-                image = load_image(dt_dir, center)
-
-            # Populate the image and steering angle arrays
-            images[idx] = preprocess(image)
-            steers[idx] = steering_angle
-
-        count += batch_size
-        if curr_bs < batch_size:
-            count = 0  # Reset the counter
-        yield images[:curr_bs], steers[:curr_bs]
-
-
-def train_model(mdl, x_train, x_valid, y_train, y_valid, model_name, cps_path='checkpoints/model-{val_loss:03f}.h5'):
-    """
-    Train a model.
-
-    :param mdl: Keras sequential or functional model
-    :param x_train: training procedure input data
-    :param x_valid: validation procedure input data
-    :param y_train: training procedure output (label) data
-    :param y_valid: validation procedure output (label data
-    :param model_name: name of the model
-    :param cps_path: checkpoints path where the trained models are stored
-    :return: None
-    """
-
-    # Checkpoint callback used to save the trained models
-    checkpoint = ModelCheckpoint(cps_path,
-                                 monitor='val_loss',
-                                 verbose=1,
-                                 save_best_only=True,
-                                 mode='auto')
-
-    # Compile the model
-    mdl.compile(loss='mse',
-                optimizer=Adam(lr=LR))
-
-    # Train the model
-    history = mdl.fit(batcher(TRAIN_DATA_DIR, x_train, y_train, B_SIZE, True),
-                      steps_per_epoch=np.ceil(len(x_train) / B_SIZE),
-                      epochs=NB_EPOCHS,
-                      validation_data=batcher(TRAIN_DATA_DIR, x_valid, y_valid, B_SIZE, False),
-                      validation_steps=np.ceil(len(x_valid) / B_SIZE),
-                      callbacks=[checkpoint],
-                      verbose=1)
-
-    # Plot the training and validation losses
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title(f'{model_name} Model Loss (learning rate: {LR})')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train_loss', 'val_loss'], loc='upper left')
+def plot_data(data, title, xlabel, ylabel):
+    plt.figure(figsize=(10, 8))
+    plt.plot(data, color='g')  # peak at 13850
+    plt.title(title, weight='bold', fontsize=16)
+    plt.xlabel(xlabel, weight='bold', fontsize=14)
+    plt.ylabel(ylabel, weight='bold', fontsize=14)
+    plt.xticks(weight='bold', fontsize=12, rotation=45)
+    plt.yticks(weight='bold', fontsize=12)
+    plt.grid(color='y', linewidth=0.5)
     plt.show()
